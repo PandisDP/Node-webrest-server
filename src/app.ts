35f52cae04460ae372c6775ee81a459cbfaffb5a
Jorge Lozano <jl@Jorges-MacBook-Pro.local>
2024-10-04 23:11:20 -0500

    First Commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
+
+
+
+(async()=>{
+    main();
+})();
+
+async function main(){
+    const  server = new Server(
+        {
+            port: envs.PORT,
+            public_path: envs.PUBLIC_PATH
+        }
+    );
+    await server.start();
+}
